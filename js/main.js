ea6a664c9d2a4b22bb47060afa9873e2740f20da
Vitalynx <selimaydi@gmail.com>
2019-02-07 20:07:17 +0100

    selims changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-$('.carousel').carousel({
-    interval: 3000,
-    cycle: true,
-    pause: false
-})
+
+$(document).ready(function () {
+    $('.regular-fade').each(function () {
+        $(this).fadeTo(100, 1);
+    });
+
+    $('.carousel').carousel({
+        interval: 3000,
+        cycle: true,
+        pause: false
+    })
+
+    $(window).scroll(function () {
+        $('.fade-content').each(function () {
+            if (!outOfBounds($(this))) {
+                $(this).stop().fadeTo(100, 1);
+            }
+
+        });
+    });
+
+    function outOfBounds($element) {
+        return $element.offset().top < $(window).scrollTop() || $element.offset().top + $element.height() > $(window).scrollTop() + $(window).height();
+    }
+
+});
